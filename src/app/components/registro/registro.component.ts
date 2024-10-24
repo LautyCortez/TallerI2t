a7c05c100f5b5c12635f6ce5a09c62342e0083ea
LautyCortez <nuevoemail@ejemplo.com>
2024-10-24 16:08:12 -0300

    Agregue cosas(No compila)

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { UsuariosModule } from 'src/app/models/usuarios/usuarios.module';
 @Component({
   selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  templateUrl: './registro.component.html'
 })
 export class RegistroComponent {
+  usuario: UsuariosModule = new usuario();
 
+  registrar() {
+    console.log('Registrando usuario:', this.usuario);
+    // Lógica para registrar al usuario en el backend
+  }
 }
