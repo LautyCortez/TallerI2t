486a3d750237a93d7a98074f64d482e91e5bf67e
Alejo Castelnovo <castelnovo12@gmail.com>
2024-11-22 14:52:23 -0300

    editar y cancelar

--- src/app/service/reserva.service.ts
+++ src/app/service/reserva.service.ts
@@ -22,6 +22,8 @@ export class ReservaService {
     return this.http.get<any[]>(`${this.apiUrl}/all`);
   }
 
+
+  
   obtenerReservaPorId(idHospedaje: number, idUsuario: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${idHospedaje}/${idUsuario}`);
   }
@@ -40,4 +42,10 @@ export class ReservaService {
     });
     return this.http.delete<void>(`${this.apiUrl}/delete/${idHospedaje}/${idUsuario}`, { headers });
   }
+
+
+  obtenerReservasPorUsuario(idUsuario: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/usuario/${idUsuario}`);
+}
+
 }
